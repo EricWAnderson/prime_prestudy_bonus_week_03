b5e112fd6d67b592f138795609f5ff62f1b3dcab
EricWAnderson <eric429@gmail.com>
2015-11-16 00:51:42 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hey! The script file is working!");
+
+function alpha(x) {
+	x += 101;
+	return x;
+}
+
+function beta(x) {
+	x += 1000;
+	return x;
+}
+
+function gamma(x) {
+	x += 10000;
+	return x;
+}
+
+console.log("Result is " + alpha(beta(gamma(10))));
